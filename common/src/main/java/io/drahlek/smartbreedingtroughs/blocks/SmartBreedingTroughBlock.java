3f50f90f1314e4350154fe0a3d0466541fde2918
Kevin King <kingnh@comcast.net>
2026-05-06 20:26:38 -0400

    - change size of block - change item inventory icon

--- common/src/main/java/io/drahlek/smartbreedingtroughs/blocks/SmartBreedingTroughBlock.java
+++ common/src/main/java/io/drahlek/smartbreedingtroughs/blocks/SmartBreedingTroughBlock.java
@@ -37,9 +37,19 @@ public class SmartBreedingTroughBlock extends BaseEntityBlock {
     public static final int SLOT_COUNT = 4;
     public static final EnumProperty<Direction> FACING = BlockStateProperties.HORIZONTAL_FACING;
     public static final MapCodec<SmartBreedingTroughBlock> CODEC = simpleCodec(SmartBreedingTroughBlock::new);
-    private static final VoxelShape SHAPE = Shapes.or(
-            box(1.0D, 0.0D, 1.0D, 15.0D, 5.0D, 15.0D),
-            box(0.0D, 5.0D, 0.0D, 16.0D, 8.0D, 16.0D)
+    private static final VoxelShape X_AXIS_SHAPE = Shapes.or(
+            box(2.0D, 0.0D, 4.0D, 14.0D, 2.0D, 12.0D),
+            box(0.0D, 2.0D, 3.0D, 16.0D, 8.0D, 5.0D),
+            box(0.0D, 2.0D, 11.0D, 16.0D, 8.0D, 13.0D),
+            box(0.0D, 2.0D, 5.0D, 2.0D, 8.0D, 11.0D),
+            box(14.0D, 2.0D, 5.0D, 16.0D, 8.0D, 11.0D)
+    );
+    private static final VoxelShape Z_AXIS_SHAPE = Shapes.or(
+            box(4.0D, 0.0D, 2.0D, 12.0D, 2.0D, 14.0D),
+            box(3.0D, 2.0D, 0.0D, 5.0D, 8.0D, 16.0D),
+            box(11.0D, 2.0D, 0.0D, 13.0D, 8.0D, 16.0D),
+            box(5.0D, 2.0D, 0.0D, 11.0D, 8.0D, 2.0D),
+            box(5.0D, 2.0D, 14.0D, 11.0D, 8.0D, 16.0D)
     );
 
     public SmartBreedingTroughBlock(BlockBehaviour.Properties properties) {
@@ -102,6 +112,6 @@ public class SmartBreedingTroughBlock extends BaseEntityBlock {
 
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return SHAPE;
+        return state.getValue(FACING).getAxis() == Direction.Axis.X ? Z_AXIS_SHAPE : X_AXIS_SHAPE;
     }
 }
