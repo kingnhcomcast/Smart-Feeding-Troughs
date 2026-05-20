0cd03f0e8994fc14033553aab6072d6d0be2849b
Kevin King <kingnh@comcast.net>
2026-05-19 21:53:50 -0400

    - add tooltip - add item texture - fix item texture for trough

--- common/src/main/java/io/drahlek/smartfeedingtroughs/items/EdibleHayBlockItem.java
+++ common/src/main/java/io/drahlek/smartfeedingtroughs/items/EdibleHayBlockItem.java
@@ -3,11 +3,22 @@ package io.drahlek.smartfeedingtroughs.items;
 import io.drahlek.dirigo.annotation.Item;
 import io.drahlek.dirigo.registrars.BlockRegistrar;
 import io.drahlek.smartfeedingtroughs.blocks.EdibleHayBlock;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
+
+import java.util.function.Consumer;
 
 @Item(id = EdibleHayBlock.NAME, creativeTab = "functional_blocks")
 public class EdibleHayBlockItem extends BlockItem {
     public EdibleHayBlockItem(Properties properties) {
         super(BlockRegistrar.blocks.get(EdibleHayBlock.NAME).get(), properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, net.minecraft.world.item.Item.TooltipContext context, TooltipDisplay display, Consumer<Component> textConsumer, TooltipFlag tooltipFlag) {
+        textConsumer.accept(Component.translatable("tooltip.smartfeedingtroughs.edible_hay_block"));
     }
 }
