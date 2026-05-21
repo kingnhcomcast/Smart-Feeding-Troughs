942ddc13068bf05f9b45204958b5725c3311a762
Kevin King <kingnh@comcast.net>
2026-05-20 22:36:40 -0400

    when hay bale is eaten, change size to reflect current charges

--- common/src/main/java/io/drahlek/smartfeedingtroughs/blocks/EdibleHayBlock.java
+++ common/src/main/java/io/drahlek/smartfeedingtroughs/blocks/EdibleHayBlock.java
@@ -6,8 +6,10 @@ import io.drahlek.smartfeedingtroughs.blocks.entity.EdibleHayBlockEntity;
 import io.drahlek.smartfeedingtroughs.blocks.entity.FeedingBlockEntity;
 import java.util.List;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.HayBlock;
@@ -16,10 +18,13 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jspecify.annotations.Nullable;
 
 @Block(id = EdibleHayBlock.NAME,
@@ -27,6 +32,15 @@ import org.jspecify.annotations.Nullable;
         registerItem = false)
 public class EdibleHayBlock extends HayBlock implements EntityBlock {
     public static final String NAME = "edible_hay_block";
+    public static final int MAX_CHARGES = 8;
+    public static final IntegerProperty CHARGES = IntegerProperty.create("charges", 1, MAX_CHARGES);
+    private static final VoxelShape[] SHAPES = new VoxelShape[MAX_CHARGES + 1];
+
+    static {
+        for (int charges = 1; charges <= MAX_CHARGES; charges++) {
+            SHAPES[charges] = box(0.0D, 0.0D, 0.0D, 16.0D, charges * 2.0D, 16.0D);
+        }
+    }
 
     public EdibleHayBlock(Properties properties) {
         super(properties
@@ -35,6 +49,9 @@ public class EdibleHayBlock extends HayBlock implements EntityBlock {
                 .strength(0.5F)
                 .sound(SoundType.GRASS)
         );
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(AXIS, Direction.Axis.Y)
+                .setValue(CHARGES, MAX_CHARGES));
     }
 
     @Override
@@ -44,11 +61,7 @@ public class EdibleHayBlock extends HayBlock implements EntityBlock {
 
     @Override
     protected List<ItemStack> getDrops(BlockState blockState, LootParams.Builder params) {
-        if (!(params.getOptionalParameter(LootContextParams.BLOCK_ENTITY) instanceof EdibleHayBlockEntity edibleHay)) {
-            return List.of();
-        }
-
-        int charges = edibleHay.getCharges();
+        int charges = blockState.getValue(CHARGES);
         return charges <= 0 ? List.of() : List.of(new ItemStack(Items.WHEAT, charges));
     }
 
@@ -59,5 +72,21 @@ public class EdibleHayBlock extends HayBlock implements EntityBlock {
                 || blockEntityType != BlockEntityRegistrar.get(EdibleHayBlock.NAME, EdibleHayBlockEntity.class)
                 ? null
                 : (BlockEntityTicker<T>) (BlockEntityTicker<EdibleHayBlockEntity>) FeedingBlockEntity::tick;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<net.minecraft.world.level.block.Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(CHARGES);
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPES[state.getValue(CHARGES)];
+    }
+
+    @Override
+    protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return getShape(state, level, pos, context);
     }
 }
